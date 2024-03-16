207cd0510d32e756ec7ff2e63577b1728d4f26d4
Jens Joachim Pedersen <krogsloekke@gmail.com>
2024-03-16 18:51:42 +0100

    Completed sampling timers

--- modules/sampling_timers.py
+++ modules/sampling_timers.py
@@ -1,16 +1,118 @@
+
+import time
+
 class sampling_timers:
 
-    import time
-    
     def __init__(self):
-
         self.sampling_timers_dict = {}
+    
+    def add(self,name,n_avg_window,init_mean=1):
+        self.sampling_timers_dict[name] = sampling_timer(n_avg_window,init_mean)
 
-    def add(self,name,n_avg_window):
-        self.sampling_timers_dict[name] = [0] * n_avg_window
-
-    def print_n(self,name):
-        print(name+": "+str(self.sampling_timers_dict[name]))
+    def start(self,name):
+        self.sampling_timers_dict[name].start()
+        
+    def stop(self,name):
+        self.sampling_timers_dict[name].stop()
+        
+    def remove(self,name):
+        del self.sampling_timers_dict[name]
+        
+    def print_x(self,name):
+        print(self.sampling_timers_dict[name].print_debug())
 
     def print_all(self):
-        print(self.sampling_timers_dict)
+        for key in self.sampling_timers_dict:
+            print(key,self.sampling_timers_dict[key].print_debug())
+
+    def print_pretty(self,en_print_label=True,label_list=["time_curr","time_mean","fps_curr","fps_mean"]):
+
+        block_size = 12
+
+        # Print labels line
+        if en_print_label:
+            s_acc = " " * block_size
+            for label in label_list:
+                s_acc = s_acc + " | " + label + " " * (block_size - len(label))
+            print(s_acc)
+            
+        # Print lines
+        for key in self.sampling_timers_dict:
+            s_acc = ""
+            s_acc = key + " " * (block_size - len(key))
+            for label in label_list:
+                self.sampling_timers_dict[key].update()
+                attr = getattr(self.sampling_timers_dict[key], label+"_s") 
+                s_acc = s_acc + " | " + attr + " " * (block_size - len(attr))
+            print(s_acc)
+            
+
+
+class sampling_timer:
+
+    def __init__(self,n_avg_window,init_mean):
+        # Mean window width
+        # Index
+        # Sum
+        # Mean
+        # FPS
+        # array with time measurements
+        self.n_avg_window = n_avg_window
+        self.idx = 0
+        self.sum_n = n_avg_window * init_mean
+        # Init array        
+        self.time_arr = [init_mean] * n_avg_window
+        # Tic
+        self.tic = -1
+        # Read out data
+        self.time_curr = init_mean
+        self.time_mean = init_mean
+        self.fps_curr = 1 / self.time_curr
+        self.fps_mean = 1 / self.time_mean
+        self.time_curr_s = ""
+        self.time_mean_s = ""
+        self.fps_curr_s = ""
+        self.fps_mean_s = ""
+        self.update()
+        
+    def start(self):
+        if self.tic != -1:
+            tic_tmp = time.time()
+            tmp_time = tic_tmp - self.tic
+            self.sum_n = self.sum_n - self.time_arr[self.idx] + tmp_time
+            self.time_arr[self.idx] = tmp_time
+            self.idx = (self.idx + 1) % self.n_avg_window
+            self.tic = tic_tmp
+        else:
+            self.tic = time.time()
+
+    def stop(self):
+        if self.tic != -1:
+            tmp_time = time.time() - self.tic
+            self.sum_n = self.sum_n - self.time_arr[self.idx] + tmp_time
+            self.time_arr[self.idx] = tmp_time
+            self.idx = (self.idx + 1) % self.n_avg_window
+            self.tic = -1
+        else:
+            print("Error! - Sampling Timer already stopped!")
+            exit()
+
+    def update(self,time_digits=3,fps_digits=3):
+        self.time_curr = self.time_arr[(self.idx + (self.n_avg_window - 1)) % self.n_avg_window]
+        self.time_mean = self.sum_n / self.n_avg_window
+        self.fps_curr = 1 / self.time_curr
+        self.fps_mean = 1 / self.time_mean
+        #self.time_curr_s = str(self.time_curr)[:time_digits]
+        #self.time_mean_s = str(self.time_mean)[:time_digits]
+        #self.fps_curr_s = str(self.fps_curr)[:fps_digits]
+        #self.fps_mean_s = str(self.fps_mean)[:fps_digits]
+        self.time_curr_s = str(round(self.time_curr,time_digits))
+        self.time_mean_s = str(round(self.time_mean,time_digits))
+        self.fps_curr_s  = str(round(self.fps_curr,fps_digits))
+        self.fps_mean_s  = str(round(self.fps_mean,fps_digits))
+
+
+    def print_debug(self):
+        self.update()
+        return "w: "+str(self.n_avg_window)+", i: "+str(self.idx)+", s: "+str(self.sum_n)+"\n"+"t_c: "+self.time_curr_s+", t_m: "+self.time_mean_s+", f_c: "+self.fps_curr_s+", f_m: "+self.fps_mean_s+"\n"+"arr: "+str(self.time_arr)
+    
--- test/test.py
+++ test/test.py
@@ -1,10 +1,57 @@
+# Import packages
+import sys
+import os
+import random
+import time
+
+# Get root directory of project to import modules
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.insert(0,parent_dir)
+
+# Import local packages / modules
 from modules import sampling_timers
 
 ts = sampling_timers.sampling_timers()
-ts.add("Calc_1",5)
+
+print("Test 1. - Test add/remove sampling timers")
+ts.add("Calc_1",4,0.2)
 ts.add("Calc_2",10)
-
-ts.print_n("Calc_1")
-ts.print_n("Calc_2")
-
+ts.print_x("Calc_1")
+ts.print_x("Calc_2")
 ts.print_all()
+ts.remove("Calc_2")
+ts.print_all()
+ts.add("Calc_3",3,0.5)
+ts.print_all()
+
+print("Test 2. - Test - time, mean time, fps, mean fps")
+for i in range(10):
+    ts.start("Calc_1")
+    ts.start("Calc_3")
+    time.sleep(random.random()*0.5)
+    ts.stop("Calc_3")
+    time.sleep(random.random()*0.5)
+ts.print_all()
+ts.stop("Calc_1")
+ts.print_all()
+#ts.stop("Calc_3")
+#print("Should not reach this place")
+
+print("Test 3. - Pretty printing")
+ts.print_pretty()
+
+print("Test 4. - Pretty printing cont 1")
+ts.remove("Calc_3")
+ts.print_pretty()
+for i in range(10):
+    time.sleep(random.random()*0.5)
+    ts.start("Calc_1")
+    ts.print_pretty(False)
+    
+print("Test 5. - Pretty printing cont 2")
+label_list = ["time_curr","fps_curr"]
+ts.print_pretty(True,label_list)
+for i in range(10):
+    time.sleep(random.random()*0.5)
+    ts.start("Calc_1")
+    ts.print_pretty(False,label_list)
