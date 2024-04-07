5ce88728f1bf6269a82f53cc57e3470befb39cd1
Jens Joachim Pedersen <krogsloekke@gmail.com>
2024-04-07 19:39:39 +0200

    Added first version of sw contrast brightness

--- test/image_stitch/test_stitcher/test_stitcher.py
+++ test/image_stitch/test_stitcher/test_stitcher.py
@@ -18,6 +18,7 @@ import cv2
 import sys
 import os
 
+import inspect
 
 
 
@@ -47,8 +48,8 @@ settings = {"confidence_threshold": 0.4}
 settings = {"detector": "orb", "crop": False, "confidence_threshold": 0.7, "nfeatures": 500, "adjuster": "ray","warper_type": "cylindrical", "compensator": "no", "blender_type": "no", "finder": "no"} # Good
 settings = {"detector": "sift", "crop": False, "confidence_threshold": 0.7, "nfeatures": 1500, "adjuster": "ray","warper_type": "cylindrical", "blender_type": "no", "finder": "no"}
 
-settings = {"detector": "sift", "crop": False, "confidence_threshold": 0.7, "nfeatures": 1500, "adjuster": "ray","warper_type": "cylindrical", "blender_type": "no", "finder": "no"}
-settings = {"detector": "orb", "crop": False, "confidence_threshold": 0.7, "nfeatures": 1500, "adjuster": "ray","warper_type": "cylindrical", "blender_type": "no", "finder": "no"}
+settings = {"detector": "sift", "crop": False, "confidence_threshold": 0.7, "nfeatures": 1500, "adjuster": "ray","warper_type": "cylindrical", "compensator": "no", "blender_type": "no", "finder": "no"}
+#settings = {"detector": "orb", "crop": False, "confidence_threshold": 0.7, "nfeatures": 1500, "adjuster": "ray","warper_type": "cylindrical", "blender_type": "no", "finder": "no"}
 
 
 
@@ -144,6 +145,34 @@ def init_stitcher(vid_stitch_en,**settings):
     else:
         return Stitcher(**settings)
 
+
+
+def sw_contrast_brightness(opt,imgs_in):
+    # Disabled
+    if opt == 0:
+        return imgs_in
+    # Two contranst brightness functions
+    imgs_out = []
+    for img in imgs_in:
+        if opt == 1:
+            # Option 1
+            #alpha = 2.0 # Contrast control
+            #beta = 10 # Brightness control
+            alpha = 5.0 # Contrast control
+            beta = -200 # Brightness control
+            imgs_out.append(cv2.convertScaleAbs(img, alpha=alpha, beta=beta))
+        elif opt == 2:
+            # Option 2
+            #contrast = 2 # Contrast control ( 0 to 127)
+            #brightness = 0 # Brightness control (0-100)
+            contrast = 2 # Contrast control ( 0 to 127)
+            brightness = 0 # Brightness control (0-100)
+            imgs_out.append(cv2.addWeighted(img, contrast, img, 0, brightness))
+        else:
+            print("inspect.stack()[0][3]: Wrong option: "+str(opt))
+            sys.exit(1)
+    return imgs_out       
+
 # Init Camera
 init_get_imgs(cam_en)
 
@@ -170,6 +199,8 @@ st.add("stitch",4)
 
 # First panorama will set registration
 imgs = get_imgs(cam_en,image_paths)
+sw_con_bright_en = 1
+imgs = sw_contrast_brightness(sw_con_bright_en,imgs)
 stitch_success = True
 try:
     panorama = stitcher.stitch(imgs)
@@ -179,10 +210,11 @@ except:
 
 # If stitching failed opne images
 if stitch_success == False:
+    for i in range(len(imgs)):
+        cv2.namedWindow("img_"+str(i), cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty("img_"+str(i), cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     while True:
         for i in range(len(imgs)):
-            cv2.namedWindow("img_"+str(i), cv2.WINDOW_NORMAL)
-            cv2.setWindowProperty("img_"+str(i), cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
             cv2.imshow("img_"+str(i),imgs[i])
             waitkey_in = cv2.waitKey(1) & 0xFF
             if waitkey_in == ord('q'): # Exit
@@ -200,6 +232,7 @@ while True:
     # Stitch images
     imgs = []
     imgs = get_imgs(cam_en,image_paths)
+    imgs = sw_contrast_brightness(sw_con_bright_en,imgs)
     st.start("stitch")
     panorama = stitcher.stitch(imgs)
     st.stop("stitch")
@@ -236,8 +269,6 @@ while True:
     if waitkey_in == ord('y'): # Compensator OFF
         stitcher.compensator = ExposureErrorCompensator("no")
     if waitkey_in == ord('b'): # Blender ON
-        #stitcher.blender = Blender()
-        #stitcher.blender = Blender("feather",1)
         stitcher.blender = Blender("multiband",1)
     if waitkey_in == ord('n'): # Blender OFF
         stitcher.blender = Blender("no")
