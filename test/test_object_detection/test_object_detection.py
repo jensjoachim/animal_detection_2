aa7b54e51aa2b8bd9cc88e998ea801aedb9e1f9c
Jens Joachim <krogsloekke@gmail.com>
2024-09-17 20:31:05 +0200

    working test object detection

--- test/test_object_detection/test_object_detection.py
+++ test/test_object_detection/test_object_detection.py
@@ -2,6 +2,13 @@
 import sys
 import os
 import cv2
+import numpy as np
+
+#from PIL import Image
+#from PIL import ImageColor
+#from PIL import ImageDraw
+#from PIL import ImageFont
+#from PIL import ImageOps
 
 
 import importlib.util
@@ -37,7 +44,8 @@ def load_model():
             TFLITE_EN   = True
             TFLITE_PC   = False
             EDGE_TPU_EN = True
-            MODEL_DIR   = '../../18_08_2022_efficientdet-lite1_e75_b32_s2000/'
+            #MODEL_DIR   = '../../18_08_2022_efficientdet-lite1_e75_b32_s2000/'
+            MODEL_DIR   = '../../../Downloads/18_08_2022_efficientdet-lite1_e75_b32_s2000/'
         else:
             TFLITE_EN   = True
             TFLITE_PC   = True
@@ -77,22 +85,22 @@ def load_model():
         if TFLITE_PC: 
             if EDGE_TPU_EN:
                 # Edge TPU TFLITE
-                core_print_info('No support for Edge TPU on PC...')
+                print('No support for Edge TPU on PC...')
                 exit()
             else:
                 # float16 TFLITE
                 interpreter = Interpreter(model_path=os.path.join(MODEL_DIR,'model_float16.tflite'))
-                core_print_info('Loading TFLITE Float16...')
+                print('Loading TFLITE Float16...')
         else:
             if EDGE_TPU_EN:
                 # Edge TPU TFLITE
                 interpreter = Interpreter(model_path=os.path.join(MODEL_DIR,'edge_tpu_2','model_default_edgetpu.tflite'),
                                           experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
-                core_print_info('Loading TFLITE for Edge TPU...')
+                print('Loading TFLITE for Edge TPU...')
             else:
                 # Default TFLITE
                 interpreter = Interpreter(model_path=os.path.join(MODEL_DIR,'model_default.tflite'))
-                core_print_info('Loading TFLITE Default...')
+                print('Loading TFLITE Default...')
                 # Allocate    
         interpreter.allocate_tensors()
         # Get model details
@@ -100,7 +108,7 @@ def load_model():
         output_details = interpreter.get_output_details()
         height = input_details[0]['shape'][1]
         width = input_details[0]['shape'][2]
-        core_print_info('H: '+str(height)+' W: '+str(width))
+        print('H: '+str(height)+' W: '+str(width))
         tflite_model_height = height
         tflite_model_width  = width
 
@@ -120,11 +128,98 @@ def load_model():
     else:
         import tensorflow as tf
         # Print Tensorflow version
-        core_print_info('Tensorflow version: '+tf.__version__)
+        print('Tensorflow version: '+tf.__version__)
         # Init Model
         detect_fn = tf.saved_model.load(MODEL_DIR+'saved_model/')
 
 load_model()
+
+# Load Labels
+category_index = {}
+labels_file = open(MODEL_DIR+'labels.txt', 'r')
+lines_labels_file = labels_file.readlines()
+i = 1
+for line in lines_labels_file:
+    category_index[i] = {'id': i, 'name': line.split('\n')[0]}
+    print("\'id\': "+str(i)+", \'name\': "+line.split('\n')[0])
+    i = i + 1
+# Define a list of colors for visualization
+np.random.seed(1931)
+COLORS = np.random.randint(25, 230, size=(i, 3), dtype=np.uint8)
+color_selection = []
+i = 1
+for color in COLORS:
+    color_selection.append('#{0:02x}{1:02x}{1:02x}'.format(color[2],color[1],color[0]))
+    print("\'id\': "+str(i)+", "+'#{0:02x}{1:02x}{1:02x}'.format(color[2],color[1],color[0]))    
+    i = i + 1
+
+
+
+# Functions - Detection
+
+def run_detector(img):
+
+    # Run detector
+
+    if TFLITE_EN:
+        
+        # Run detector
+        frame_rgb = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (tflite_model_width, tflite_model_height))
+        input_data = np.expand_dims(frame_resized, axis=0)
+
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
+
+        # Perform the actual detection by running the model with the image as input
+        interpreter.set_tensor(input_details[0]['index'],input_data)
+        interpreter.invoke()
+
+        # Retrieve detection results
+        boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+        classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
+        scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+        
+        # Add one to classes to match MODEL
+        for i in range(len(classes)):
+            classes[i] = classes[i] + 1
+        
+        # Convert to NP
+        boxes = np.array(boxes)
+        classes = np.array(classes).astype(np.int64)
+        scores = np.array(scores)
+
+    else:
+
+        # Run detector
+        image_np = np.array(img.copy())
+        input_tensor = tf.convert_to_tensor(image_np)
+        input_tensor = input_tensor[tf.newaxis, ...]
+        detections = detect_fn(input_tensor)
+    
+        # Normalize boxes
+        boxes   = []
+        for box in tf.get_static_value(detections[0][0]):
+            boxes.append([box[0]/image_np.shape[0],
+                          box[1]/image_np.shape[1],
+                          box[2]/image_np.shape[0],
+                          box[3]/image_np.shape[1]])  
+        boxes = np.array(boxes)
+        # Convert Classes to integers
+        classes = tf.get_static_value(detections[2][0]).astype(np.int64)
+        # Scores
+        scores  = tf.get_static_value(detections[1][0])
+        
+    # Load in dict
+    detections = {}
+    detections['detection_boxes'] = boxes
+    detections['detection_scores'] = scores
+    detections['detection_classes'] = classes
+
+    return detections
+
+
 
 
 # Get root directory of project to import modules
@@ -165,6 +260,9 @@ while running:
     st.stop("read_image")
     # Object detection
     #
+
+    print(run_detector(img))
+    
     # Show image
     if restart_imshow_window == True:
         cv2.namedWindow('img', cv2.WINDOW_NORMAL)
@@ -208,6 +306,12 @@ while running:
             cam_ctrl.zoom_img_add("out")
         else:
             cam_ctrl.zoom_cursor_in_out("out")
+    #elif waitkey_in == ord('1'): # DBG Info #1
+    #    scaler_crop = cam_ctrl.get_scaler_crop()
+    #    print("scaler_crop: "+str(scaler_crop))
+    #elif waitkey_in == ord('2'): # DBG Info #2
+    #    lens_position = cam_ctrl.get_lens_position()
+    #    print("lens_position: "+str(lens_position))
     elif waitkey_in == ord('1'): # DBG Info #1
         scaler_crop = cam_ctrl.get_scaler_crop()
         print("scaler_crop: "+str(scaler_crop))
